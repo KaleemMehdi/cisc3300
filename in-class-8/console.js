fca5662c9ee9a430afa84dacd9f912c2bc537e07
Kaleem Mehdi <kaleemmehdi@Kaleems-MacBook-Air.local>
2025-02-10 17:05:32 -0500

    in class 8

---
+++ in-class-8/console.js
@@ -0,0 +1,9 @@
+console.log(1 + '4') // the output is 14 because it's a console message, so it ouputs the number 1 followed by the string '4'
+
+console.log(Number(true)) // the ouput is 1 because true is equivalent to 1 
+console.log(Number(false)) // the ouput is 0 because false is equivalent to 0
+
+console.log(Boolean(1)) // the output is true because 1 is equivalent to true, there is something. 
+console.log(Boolean(0)) // the output is false because 0 is equivalent to false, there's nothing.
+console.log(Boolean("0")) // the output is equivalent to true because "0" is a string which means there's something.
+

1d008bf906bfc90ea0d8fe5f884b192ad4548179
Kaleem Mehdi <kaleemmehdi@Kaleems-MacBook-Air.local>
2025-03-10 13:40:22 -0400

    homework 7

---
+++ homework7/index.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace homework7;
+
+require "../homework7/controllers/UserController.php";
+
+use homework7\controllers\UserController;
+
+class Router {
+
+    public function userRoutes() {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $userController = new UserController();
+            $userController->saveNote();
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $userController = new UserController();
+            $userController->viewNote();
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            require 'cisc3300/homework7/form.html';
+            exit();
+        }
+
+    }
+}
+
+$router = new Router();
+$router->userRoutes();
